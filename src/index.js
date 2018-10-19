ed6a9b4555e87fb327187c53c5a1fd47c404eb9a
Richard Chen <rchenmedia@gmail.com>
2018-10-18 23:15:46 -0400

    full code with redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';        //view library, core package dom manipulation
-import ReactDOM from 'react-dom'; //can plug and play different packages based on screen, like native for mobile
-import './index.css';             //same directory stylesheet
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import 'tachyons';
 
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import { requestRobots, searchRobots } from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.register();
+import './index.css';
+
+const logger = createLogger()
+
+const rootReducers = combineReducers({requestRobots, searchRobots})
+
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
